6500824393544626b7774c8a03a92ec2b6798c71
Josh Newton <josh.newton@learnsci.co.uk>
2020-03-05 13:29:43 +0000

    Scaffold out grid and hero components

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,21 +1,48 @@
 import { h, render, Component } from 'preact';
 
+const POPULAR_URL = '//localhost:6060/offers/popular';
+const HEADERS = { headers: { 'X-ApiKey': 'letmein!' } };
+
+
 class Grid extends Component {
 
   constructor() {
     super();
     this.state = {
-      data: 'Grid Component'
+      data: null,
+      isLoaded: false,
+      error: null
     }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    fetch(POPULAR_URL, HEADERS)
+      .then(response => response.json())
+      .then((data) => this.setState({ data: data, isLoading: false }))
+      .catch((error) => this.setState({ error: error }));
+  }
 
   render() {
-    const { data } = this.state;
+    const { data, isLoaded, error } = this.state;
+    console.log(data);
+
+    // Something went wrong
+    if (error !== null) {
+      return (
+        <p>Error loading grid data: { error }</p>
+      );
+    }
+    // Display loading until fetch data has finished
+    if (isLoaded) {
+      return (
+        <p>Loading animation here!</p>
+      );
+    }
+
+    // Everything is working...
     return (
       <div className="Grid">
-        <p>{ data }</p>
+        <p>Grid Component</p>
       </div>
     );
   }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,21 +1,48 @@
 import { h, render, Component } from 'preact';
 
+const HERO_URL = '//localhost:6060/offers/category';
+const HEADERS = { headers: { 'X-ApiKey': 'letmein!' } };
+
+
 class Hero extends Component {
 
   constructor() {
     super();
     this.state = {
-      data: 'Hero Component'
+      data: null,
+      isLoaded: false,
+      error: null
     }
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    fetch(HERO_URL, HEADERS)
+      .then(response => response.json())
+      .then((data) => this.setState({ data: data, isLoading: false }))
+      .catch((error) => this.setState({ error: error }));
+  }
 
   render() {
-    const { data } = this.state;
+    const { data, isLoaded, error } = this.state;
+    console.log(data);
+
+    // Something went wrong
+    if (error !== null) {
+      return (
+        <p>Error loading hero data: { error }</p>
+      );
+    }
+    // Display loading until fetch data has finished
+    if (isLoaded) {
+      return (
+        <p>Loading animation here!</p>
+      );
+    }
+
+    // Everything is working...
     return (
       <div className="Hero">
-        <p>{ data }</p>
+        <p>Hero Component</p>
       </div>
     );
   }
