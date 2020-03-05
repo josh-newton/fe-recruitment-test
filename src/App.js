03016c1c1463b6525385a33e02351f2cec2e0fd0
Josh Newton <josh.newton@learnsci.co.uk>
2020-03-05 13:19:42 +0000

    Using js instead of ts, added App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { h, render } from 'preact';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default App;
--- src/client.ts
+++
@@ -1,3 +0,0 @@
-import { h } from 'preact';
-
-// Write something awesome
